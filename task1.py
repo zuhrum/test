7a1e489742ce469207684bc5d477ed17a22c6fb9
Zuhrum <Zuhrum@mail.ru>
2020-08-18 05:56:33 +0700

    тест

---
+++ task1.py
@@ -0,0 +1,10 @@
+first_number = 123
+print(first_number)
+first_string = "string"
+print(first_string)
+
+first_string = str(input("Введите первую строку: "))
+second_string = str(input("Введите вторую строку: "))
+third_number = int(input("Введите первое число: "))
+fourth_number = int(input("Введите первое число: "))
+print(first_string, second_string, third_number, fourth_number)
